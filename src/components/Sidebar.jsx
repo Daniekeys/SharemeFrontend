ad15099e1c21676bdd1f1f93bfa69599ab430bd0
Daniekeys <starlordflash@gmail.com>
2022-01-29 12:42:26 -0800

    adjustment to login screen changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,8 @@ import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../assets/logo.png';
 import { categories } from '../utils/data';
 
-const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize mb-2 cursor-pointer';
-const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize mb-2';
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize mb-4 md:mb-0 cursor-pointer';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize mb-4 md:mb-0 cursor-pointer';
 
 const Sidebar = ({ closeToggle, user }) => {
   const handleCloseSidebar = () => {
